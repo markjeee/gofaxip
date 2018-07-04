fef9460bf453d21cc892a7023f2d0ec3befc103f
michael <michael.suarez@synacy.com>
2018-07-04 18:09:30 +0800

    Read Header from tagline field of Hylafax job
    We need to use the tagline from the Hylafax job instead of header value
    in gofax.conf.
    
    Signed-off-by: michael <michael.suarez@synacy.com>

--- gofaxsend/sendqfile.go
+++ gofaxsend/sendqfile.go
@@ -68,7 +68,7 @@ func SendQfile(qfilename string) (int, error) {
 	faxjob.Number = fmt.Sprint(gofaxlib.Config.Gofaxsend.CallPrefix, qf.GetString("external"))
 	faxjob.Cidnum = gofaxlib.Config.Gofaxsend.FaxNumber //qf.GetString("faxnumber")
 	faxjob.Ident = gofaxlib.Config.Freeswitch.Ident
-	faxjob.Header = gofaxlib.Config.Freeswitch.Header
+	faxjob.Header = qf.GetString("tagline")
 	faxjob.Gateways = gofaxlib.Config.Freeswitch.Gateway
 
 	if ecmMode, err := qf.GetInt("desiredec"); err == nil {
